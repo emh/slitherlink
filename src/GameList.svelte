29dc2679a6a4bf5ff9e94397702ad44be1e12224
Evan Haveman <evanhaveman@gmail.com>
2021-03-26 20:52:01 -0700

    save played game state to localstorage show when created and last played in game list sort game list

--- src/GameList.svelte
+++ src/GameList.svelte
@@ -1,39 +1,73 @@
 <script>
-import { onMount } from "svelte";
+    import { onMount } from "svelte";
+
+    const SEED_GAMES = [
+        '10-4f3qd-e4ap-b4p-2gvrm-23dji-19tz-4vum6-3o4y-15xo7-39hdm',
+        '10-8df9-4ozj2-50x7-qie2-p4m9-91j6-xkca-50wi-4o37v-p488'
+    ];
+
     let games;
 
     function loadGames() {
-        games = JSON.parse(localStorage.getItem('games'));
+        const n = localStorage.length;
 
-        if (!games || games.length === 0) {
+        if (n === 0) {
             seedGames();
+            return;
+        }
+
+        games = {};
+
+        for (let i = 0; i < localStorage.length; i++) {
+            const id = localStorage.key(i);
+
+            games[id] = JSON.parse(localStorage.getItem(id));
         }
     }
 
     function seedGames() {
-        localStorage.setItem('games', JSON.stringify({
-            '10-4f3qd-e4ap-b4p-2gvrm-23dji-19tz-4vum6-3o4y-15xo7-39hdm': {},
-            '10-8df9-4ozj2-50x7-qie2-p4m9-91j6-xkca-50wi-4o37v-p488': {}
-        }));
+        SEED_GAMES.forEach((id) => {
+            const gameStr = JSON.stringify({ id, createdAt: Date.now(), state: [] });
+
+            localStorage.setItem(id, gameStr);
+        });
 
         loadGames();
+    }
+
+    function sortGames(games) {
+        if (!games) {
+            return [];
+        }
+
+        return Object.values(games).sort((a, b) => (b.updatedAt || b.createdAt) - (a.updatedAt || a.createdAt));
+    }
+
+    function timeAgo(t) {
+        if (!t) {
+            return 'never';
+        }
+
+        const d = (Date.now() - t) / 1000;
+
+        if (d < 60) return 'just now';
+        if (d < 15 * 60) return 'moments ago';
+        if (d < 30 * 60) return 'less than a half hour ago';
+        if (d < 60 * 60) return 'less than an hour ago';
+        if (d < 3 * 60 * 60) return 'a few hours ago';
+        if (d < 24 * 60 * 60) return 'today';
+        if (d < 2 * 24 * 60 * 60) return 'yesterday';
+        if (d < 7 * 24 * 60 * 60) return 'this week';
+        if (d < 14 * 24 * 60 * 60) return 'last week';
+        if (d < 28 * 24 * 60 * 60) return 'a few weeks ago';
+
+        return 'a long ass time ago';
     }
 
     onMount(loadGames);
 </script>
 
 <style>
-    :global(*) {
-        box-sizing: border-box;
-        font-family: sans-serif;
-    }
-
-    h1 {
-        font-size: 48px;
-        letter-spacing: 4px;
-        font-weight: 100;
-    }
-
     h3 {
         font-weight: 100;
     }
@@ -43,16 +77,19 @@ import { onMount } from "svelte";
         padding: 0;
     }
 
-    .app {
-        padding-top: 100px;
-        padding-left: 100px;
+    .stats {
+        color: lightblue;
+    }
+
+    .time {
+        color: orange;
     }
 </style>
 
 <h3>saved games</h3>
 
 <ul>
-    {#each Object.keys(games || {}) as id}
-        <li><a href='#{id}'>{id}</a></li>
+    {#each sortGames(games) as game}
+        <li><a href='#{game.id}'>{game.id}</a> <span class="stats">played <span class="time">{timeAgo(game.updatedAt)}</span> created <span class="time">{timeAgo(game.createdAt)}</span></span></li>
     {/each}
 </ul>
--- src/GamePlayer.svelte
+++ src/GamePlayer.svelte
@@ -2,39 +2,79 @@
 	import Cell from './GameCell.svelte';
 	import Dot from './Dot.svelte';
 	import Edge from './Edge.svelte';
+    import { onMount } from 'svelte';
+    import { encode } from './encoder.js';
 
     export let board = null;
 
-    let cols = board[0].length;
-    let rows = board.length;
+    const id = encode(board);
 
-    let game = new Array(2 * rows + 1);
+    let game = {
+        state: []
+    };
 
-    for (let i = 0; i < game.length; i++) {
-        game[i] = new Array(2 * cols + 1);
+    function initializeGameState() {
+        let cols = board[0].length;
+        let rows = board.length;
 
-        for (let j = 0; j < game[i].length; j++) {
-            if (i % 2 === 0) {
-                game[i][j] = 0;
-            } else {
-                if (j % 2 === 0) {
-                    game[i][j] = 0;
+        const state = new Array(2 * rows + 1);
+
+        for (let i = 0; i < state.length; i++) {
+            state[i] = new Array(2 * cols + 1);
+
+            for (let j = 0; j < state[i].length; j++) {
+                if (i % 2 === 0) {
+                    state[i][j] = 0;
                 } else {
-                    game[i][j] = board[(i - 1) / 2][(j - 1) / 2];
+                    if (j % 2 === 0) {
+                        state[i][j] = 0;
+                    } else {
+                        state[i][j] = board[(i - 1) / 2][(j - 1) / 2];
+                    }
                 }
             }
+        }
+
+        game.state = state;
+
+        saveGameState();
+    }
+
+    function saveGameState() {
+        game.updatedAt = Date.now();
+
+        localStorage.setItem(id, JSON.stringify(game));
+    }
+
+    function loadGameState() {
+        const gameStr = localStorage.getItem(id);
+
+        game = JSON.parse(gameStr);
+
+        if (game.state.length === 0) {
+            initializeGameState();
         }
     }
 
     function handleEdgeClick(i, j) {
-        let state = game[i][j];
+        let edgeState = game.state[i][j];
 
-        game[i][j] = (state + 1) % 3;
+        game.state[i][j] = (edgeState + 1) % 3;
+
+        saveGameState();
     }
 
     function handleBackClick() {
         location.hash = '';
     }
+
+    function handleRestartClick() {
+        initializeGameState();
+    }
+
+    onMount(() => {
+        loadGameState();
+    });
 </script>
 
 <style>
@@ -52,7 +92,7 @@
 </style>
 
 <div class="board">
-    {#each game as row, i}
+    {#each game.state as row, i}
         <div class="row">
             {#each row as cell, j}
                 {#if i % 2 === 0 && j % 2 === 0}
@@ -70,3 +110,4 @@
 </div>
 
 <button on:click={handleBackClick}>back</button>
+<button on:click={handleRestartClick}>restart</button>
